cae4b0b4c5c1c71a3202d29b4665c31f4c7b0878
PhiBao <kiter0211@gmail.com>
2021-07-29 04:47:37 +0000

    Basic sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+  
   def new
     @user = User.new
   end
+  
+  def show
+  end
+  
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      flash[:success] = t("users.new.welcome")
+      redirect_to @user
+    else 
+      render :new
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :address,
+                                   :phone_number, :date_of_birth,
+                                   :password, :password_confirmation)
+    end
+    
+    # Before filters
+    
+    # Find a user
+    def load_user
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:danger] = t("users.shared.not_found")
+      redirect_to root_url
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid sign up information" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: { name: "",
+                                 email: "user@invalid",
+                                 password: "foo",
+                                 password_confirmation: "bar" } }
+    end
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
+  end
+  
+  test "valid sign up information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "Valid user",
+                                         email: "validuser@valid.com",
+                                         address: "KTX Bach Khoa",
+                                         phone_number: "1234567890",
+                                         date_of_birth: "1999/01/01",
+                                         password: "foobar",
+                                         password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+  end
+end
