58b5e38f297e6c75b4517dabf24b98939fc8e56b
vanlucphan <vanlucbs1997@gmail.com>
2021-08-06 10:45:22 +0700

    Manage tour

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2021_08_03_030343) do
     t.text "description"
     t.boolean "status"
     t.integer "tour_amount"
+    t.integer "cur_amount", default: 0, null: false
     t.date "date_begin"
     t.date "date_end"
     t.float "price"
@@ -32,10 +33,9 @@ ActiveRecord::Schema.define(version: 2021_08_03_030343) do
     t.date "date_of_birth"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "remember_digest"
     t.string "provider"
     t.string "uid"
