a91cf44795b7898a15661a13ed1ce60252dd0cd5
Mưa Phi Bảo Trần <PhiBao>
2021-08-15 14:08:31 +0700

    Improve some views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return the full title on a per-page basis
   def full_title(page_title = '')
-    base_title = "Booking Tours"
+    base_title = t("global.name")
     if base_title.empty?
       base_title
     else
--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,5 +1,13 @@
 module BookingsHelper
   def total_price bookings
-    (bookings.pluck(:total).sum / 100).to_f
+    (bookings.pluck(:total).sum).to_f
+  end
+
+  def currency booking
+    Stripe::Price.retrieve(booking.stripe_price_id)&.currency
+  end
+
+  def price booking
+    (Stripe::Price.retrieve(booking.stripe_price_id)&.unit_amount).to_f
   end
 end
