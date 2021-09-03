2abef38bd9903e8fbef124b5f9cacba3ca727539
vanlucphan <vanlucbs1997@gmail.com>
2021-09-03 17:08:48 +0700

    multi-level menu

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  belongs_to  :parent_menu_item, class_name: 'Category', optional: true
+  has_many    :child_menu_items, class_name: 'Category', foreign_key: :parent_id, dependent: :destroy
   validates :category_name, presence: true, length: { maximum: 50}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_08_28_151024) do
+ActiveRecord::Schema.define(version: 2021_09_03_094108) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_08_28_151024) do
     t.string "category_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parent_id"
   end
 
   create_table "comments", force: :cascade do |t|
