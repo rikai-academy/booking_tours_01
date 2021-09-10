9d33fab361f738bf6859c5617ebbfd5380dd786c
vanlucphan <vanlucbs1997@gmail.com>
2021-09-10 09:53:26 +0700

    Test model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_08_28_151024) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
